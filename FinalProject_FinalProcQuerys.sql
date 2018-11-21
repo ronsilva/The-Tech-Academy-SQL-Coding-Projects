0755ad1db8ac8b13270be4876bf42410ef5e5297
Ron Silva <ron_silva@outlook.com>
2018-11-20 20:12:23 -0800

    Final SQL Project

---
+++ FinalProject_FinalProcQuerys.sql
@@ -0,0 +1,16 @@
+USE db_Library
+GO
+EXEC [LP].[uspBorrowersNoBook]
+GO
+EXEC [LP].[uspBorrowersWithMoreThan5Books]
+GO
+EXEC [LP].[uspCentralBranchBooksByStephenKing]
+GO
+EXEC [LP].[uspFindLostTribe]
+GO
+EXEC [LP].[uspFindLostTribeEverywhere]
+GO
+EXEC [LP].[uspSharptownBorrowerDueToday]
+GO
+EXEC [LP].[uspTotalNumOfBooksLoanedByBranch]
+GO
